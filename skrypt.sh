0c3156d1e54a993958fe9cec87ea5f140e3f51dd
Klara Slupska <klaraslupska@gmail.com>
2025-01-17 07:01:21 -0500

    commit 1

---
+++ skrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir ./Powtorzenie
+cd Powtorzenie
+mkdir ./"Kartki Swiateczne" Pozostale "Prace domowe" Zadania
+cd "Kartki Swiateczne"
+mkdir ./Kotki Pieski
+cd ..
+mkdir ./Pozostale/"Listy zakupow"
+cd ./Pozostale/"Listy zakupow"
+mkdir "Swieta 2022" "Swieta 2023"
+cd ..
+mkdir Przepisy
+cd ..
+
